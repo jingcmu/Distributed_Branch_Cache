d518bdbb7b516d9ec3a77f56defa1942f87e3cee
Jacky Chen <jingchen@cmu.edu>
2014-04-10 12:42:42 -0700

    add file combination

--- filemanage.py
+++ filemanage.py
@@ -1,3 +1,4 @@
+import os
 
 class FileManager:
     def __init__( self, size, debug=False ):
@@ -8,9 +9,7 @@ class FileManager:
     def openfile(self, filename):
         self.filehandler = open(filename)
 
-	import os
-
-	def split(self, pathfilename, chunksize):
+	def split(pathfilename, chunksize):
 		#split a file into many small files with chunksize bytes
 		#the small files will be put into \tmp under local path
 		statinfo = os.stat(pathfilename)
@@ -18,11 +17,11 @@ class FileManager:
 		tmppath = path + '\\tmp'
 		if not os.path.exists(tmppath):
 			os.mkdir(tmppath)
-		print "file size: %d(mb)" % (statinfo.st_size/(1024*1024))
+		print "file size: %d(kb)" % (statinfo.st_size/(1024))
 		with open(pathfilename, "rb") as f:
 			index = 0
 			while True:
-				chunk = f.read(chunksize)
+				chunk = f.read(chunksize * 1024)
 				if(chunk):
 					fn = "%s\%s.part.%d" % (tmppath, filename, index)
 					index = index + 1
@@ -31,6 +30,40 @@ class FileManager:
 						fw.write(chunk)
 				else:
 					break
+		print "split finished"
+
+	def combine(pathfilename, filesize, chunksize):
+		# pathfilename: this is the file path outside \tmp folder
+		# filesize: how many bytes are in the file, this should be known beforehand
+		# chunksize: how many kb for one chunk, default 512 kb
+		# brief explain: all the temporary files are in the \tmp folder and after combining the tmp files
+		# the tmp files will be destroied
+		filenum = filesize/(chunksize*1024) + 1
+		path, filename = os.path.split(pathfilename)
+		tmppath = path + '\\tmp'
+		tmppathfilename = tmppath + '\\' + filename
+		with open(pathfilename, "ab+") as fw:
+			for i in xrange(filenum):
+				filename = tmppathfilename + ".part." + str(i)
+				if os.path.exists(filename):
+					with open(filename, "rb") as f:
+						chunk = f.read(chunksize*1024)
+						if(chunk):
+							print "absorbing ", filename
+							fw.write(chunk)
+							f.close()
+				else:
+					print "missing part " + str(i)
+					break
+		#destroy the temporary files
+		for i in xrange(filenum):
+				filename = tmppathfilename + ".part." + str(i)
+				if os.path.exists(filename):
+					os.remove(filename)
+					print "destroyed ", filename
+				else:
+					print "missing part " + str(i)
+		print "combine finished"
 
     #close a file
     def close( self ):
